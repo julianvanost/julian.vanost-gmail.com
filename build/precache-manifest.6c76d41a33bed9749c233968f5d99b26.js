acd9f69ffde8a5440f9e0bb9b5b13989fd3cd671
Julian <Van Ost>
2020-04-02 14:12:04 -0700

    adds deployed GH-PAGES evironment to project

---
+++ build/precache-manifest.6c76d41a33bed9749c233968f5d99b26.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12356d200da0a0fddac21abf5219b638",
+    "url": "/react-app-portfolio-update/index.html"
+  },
+  {
+    "revision": "a4ca07df8cbf8d4ab550",
+    "url": "/react-app-portfolio-update/static/css/main.8ed62b97.chunk.css"
+  },
+  {
+    "revision": "4a938c5a2e9396802f0c",
+    "url": "/react-app-portfolio-update/static/js/2.f6dc999f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-portfolio-update/static/js/2.f6dc999f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ca07df8cbf8d4ab550",
+    "url": "/react-app-portfolio-update/static/js/main.aed991fe.chunk.js"
+  },
+  {
+    "revision": "a827b5458c6b360c4825",
+    "url": "/react-app-portfolio-update/static/js/runtime-main.4334ffb4.js"
+  },
+  {
+    "revision": "e8ba8328ebaee74b2b6266fcc3c76c92",
+    "url": "/react-app-portfolio-update/static/media/AbutrackLogo.e8ba8328.png"
+  },
+  {
+    "revision": "e1c174aa101ce4dd182f1a12ab9685c7",
+    "url": "/react-app-portfolio-update/static/media/CartnerGolf.e1c174aa.png"
+  },
+  {
+    "revision": "3c3968d89383d7a6671c7e7fd3ede3de",
+    "url": "/react-app-portfolio-update/static/media/ci-logo.3c3968d8.png"
+  },
+  {
+    "revision": "0be9b6c2abe047cdcf180e7b97dd3877",
+    "url": "/react-app-portfolio-update/static/media/profilePicture.0be9b6c2.jpg"
+  }
+]);
